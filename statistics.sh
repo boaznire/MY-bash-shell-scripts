a64effaad2af54a1dfd68eedffef440531a9f2cb
boaznire <84347993+boaznire@users.noreply.github.com>
2021-07-08 21:49:00 +0300

    Update statistics.sh

--- statistics.sh
+++ statistics.sh
@@ -1,6 +1,4 @@
 # MY-bash-shell-scripts
-personal implementation and my first version of UBUNTU opening loging spript presents a basic information about our linux system sudited for Debian and Ubuntu
-In order to get the scripts results you will need to run multipescripts.sh which will run the main script statistics.sh
 
 !/bin/bash
 
