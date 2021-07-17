17adbfe016bc9746c0cbb5886cb159c45052b551
boaznire <84347993+boaznire@users.noreply.github.com>
2021-07-18 00:52:35 +0300

    Update automatiupdate.sh

--- automatiupdate.sh
+++ automatiupdate.sh
@@ -25,24 +25,26 @@ bash /tmp/upgrade1.sh > /path/updates_$(date +%d-%m-%Y).log
 #The script will double check, the upgradable DB should be empty in this stage
 sudo apt list --upgradable > /tmp/doublecheck.txt
 
-FILESIZE1=$(du /tmp/doublecheck.txt | awk '{print $1}')
-FILESIZE2=$(du /tmp//upgrade1.txt | awk '{print $1}')
+FILESIZE1=$( wc -l /tmp/doublecheck.txt | awk '{print $1}')
+FILESIZE2=$( wc -l /tmp/upgrade1.sh | awk '{print $1}')
 
-if [ -s /tmp/upgrade1.txt ];then#In case that any error will occure and prevent the packge/s to be installed the script to modify 
+if [ -s /tmp/upgrade1.txt ];then#
+count=$(wc -l /tmp/upgrade1.txt | awk '{print $1}')
+echo
+fi
+
+#In case that any error will occure and prevent the packge/s to be installed the script to modify 
 #If the packges installed successfully we will receive email with the list of packges installed and how many
-if [ $FILESIZE1 -gt 4 -a $FILESIZE2 -gt 0 ];then
+if [ $FILESIZE1 -gt 1 ];then
 echo "Please check for the root cause!" | mail -s "Your Ubuntu server was not updated due to error" username@domain.com
 fi
 
-if [ $FILESIZE2 -eq 0 ];then
-
- echo "No updates" | mail -s "Your Ubuntu server has been updated" username@domain.com
+if [ $FILESIZE2 -eq 1 ];then
+echo "No updates" | mail -s "Your server has been updated" username@domain.com
 else 
 sudo echo "Total updates: $count" >> /tmp/upgrade1.txt;
-cat /tmp/upgrade1.txt | mail -s "Your ubuntu server has been updated" username@domain.com
+cat /tmp/upgrade1.txt | mail -s "Your server has been updated" username@domain.com
 fi
 
-count1=$(wc -l /tmp/upgrade1.txt | awk '{print $1}')
-echo
-fi
+
 
